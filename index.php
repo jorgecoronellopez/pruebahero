9354a0f82150cd816d855ff22b24beea0e7e04e7
jorgecoronel <jorgecoronellopez1@gmail.com>
2021-11-03 18:26:28 -0600

    first commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Php en heroku</title>
+  </head>
+  <body>
+    <?php 
+    echo "Este mensaje esta hecho en php";
+     ?>
+  </body>
+</html>
